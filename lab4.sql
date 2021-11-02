65070042e437ee804f4dbde89ca5f8edd30ed084
hungryhost <hungryhost@icloud.com>
2021-11-02 06:38:08 +0300

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,86 +1,73 @@
-create or replace function children_owners(
-date1 date,
-date2 date default current_date) -- если не задана вторая дата, то п.у. текущая
-returns char(18) as $$
-declare
-    return_statement char(18); -- переменная для выводимого сообщения
-    tmp date; -- временная переменная
-BEGIN
-    if(date1 > date2) then  -- проверка, какая дата больше для корректного результата
-        tmp = date1;
-        date1 = date2;
-        date2 = date1;
-    end if;
-
-    if(age(date2, date1) <= interval '18 years 0 months 0 day')
-    then
-        return_statement = 'несовершеннолетний';
-    else
-        return_statement = to_char(DATE(date1 + interval '18 years 0 months 1 day'),'DD/MM/YYYY' );
-    end if;
-    return return_statement;
-END;$$
-language plpgsql;
-
-
-create or replace function owners_by_func(n_numBuilding numeric(8), regiment numeric(1))
-returns void as $$
-    DECLARE
-        row record;
-        flag boolean  = false;
-BEGIN
-    -- Очищение таблицы, если необходимо пользователю --
-    if(regiment = 0) then
-        truncate owners_from_func;
-    end if;
-    for row in
-        select a.idbuiding as building,
-                        o.startown as Started,
-                        ow.fio as FIO,
-                        o.part as PART,
-                        o.flat as FLAT,
-                        ow.born as BORN
-        from adress a join ownership o on a.idbuiding = o.idbuilding
-             join owners ow on o.owner = ow.id
-        where
-              -- подзапрос на количество владельцев --
-        ((select distinct count(flat)
-            from ownership p where
-                a.idbuiding = p.idbuilding
-                and o.flat = p.flat
-                and p.idbuilding = n_numBuilding
-                and p.endown is null)>=10 or
-            -- подзапрос на владельцев, которые несовершеннолетние --
-        (select distinct children_owners(born)
-            from owners ow where
-                ow.id = o.owner
-                and a.idbuiding = n_numBuilding
-                and o.idbuilding = n_numBuilding
-                and o.endown is null
-                ) = 'несовершеннолетний')
-        group by a.idbuiding, o.startown, ow.fio, o.part, o.flat, ow.born
-    LOOP
-        if(children_owners(row.BORN) != 'несовершеннолетний') then
-        -- insert операция в таблицу owners_from_func --
-            insert into owners_from_func values (row.building,
-                                             row.Started,
-                                             row.FIO,
-                                             row.PART,
-                                             row.FLAT, true);
-            flag = true;
-        else
-             insert into owners_from_func values (row.building,
-                                             row.Started,
-                                             row.FIO,
-                                             row.PART,
-
-                                             row.FLAT, false);
-             flag = true;
-        end if;
-    END LOOP;
-    if (flag is false) then -- Проверка, были ли найдены записи
-         raise info 'Не найдено';
-    end if;
-END;$$
-LANGUAGE plpgsql;
-drop function owners_by_func(n_numBuilding numeric, regiment numeric);
+CREATE
+OR
+replace FUNCTION children_owners( date1 date, date2 date DEFAULT CURRENT_DATE) -- если не задана вторая дата, то п.у. текущая
+returns                  char(18) AS $$DECLARE return_statement CHAR(18); -- переменная для выводимого сообщенияtmp                      date;     -- временная переменнаяBEGIN
+  IF(date1 > date2) then -- проверка, какая дата больше для корректного результата
+  tmp = date1;
+  date1 = date2;
+  date2 = date1;
+ENDIF;IF(Age(date2, date1) <= interval '18 years 0 months 0 day') THEN
+return_statement = 'несовершеннолетний';
+else return_statement = to_char(date(date1 + interval '18 years 0 months 1 day'),'DD/MM/YYYY' );ENDIF;RETURN return_statement;END;$$ language plpgsql;CREATE
+OR
+replace FUNCTION owners_by_func(n_numbuilding numeric(8), regiment numeric(1))
+returns void AS $$DECLARE row RECORD;flag boolean = false;BEGIN
+  -- Очищение таблицы, если необходимо пользователю --
+  IF(regiment = 0) then
+  TRUNCATE owners_from_func;
+ENDIF;FOR row INSELECT   a.idbuiding AS building,
+         o.startown  AS started,
+         ow.fio      AS fio,
+         o.part      AS part,
+         o.flat      AS flat,
+         ow.born     AS born
+FROM     adress a
+JOIN     ownership o
+ON       a.idbuiding = o.idbuilding
+JOIN     owners ow
+ON       o.owner = ow.id
+WHERE
+         -- подзапрос на количество владельцев --
+         ((
+                                  SELECT DISTINCT Count(flat)
+                                  FROM            ownership p
+                                  WHERE           a.idbuiding = p.idbuilding
+                                  AND             o.flat = p.flat
+                                  AND             p.idbuilding = n_numbuilding
+                                  AND             p.endown IS NULL)>=10
+         OR
+                  -- подзапрос на владельцев, которые несовершеннолетние --
+                  (
+                                  SELECT DISTINCT Children_owners(born)
+                                  FROM            owners ow
+                                  WHERE           ow.id = o.owner
+                                  AND             a.idbuiding = n_numbuilding
+                                  AND             o.idbuilding = n_numbuilding
+                                  AND             o.endown IS NULL ) = 'несовершеннолетний')
+GROUP BY a.idbuiding,
+         o.startown,
+         ow.fio,
+         o.part,
+         o.flat,
+         ow.born
+loopIF(Children_owners(row.born) != 'несовершеннолетний') then
+-- insert операция в таблицу owners_from_func --
+INSERT INTO owners_from_func VALUES
+            (
+                        row.building,
+                        row.started,
+                        row.fio,
+                        row.part,
+                        row.flat,
+                        true
+            );flag = true;ELSEINSERT INTO owners_from_func VALUES
+            (
+                        row.building,
+                        row.started,
+                        row.fio,
+                        row.part,
+                        row.flat,
+                        false
+            );flag = true;ENDIF;END
+loop;IF (flag IS false) THEN -- Проверка, были ли найдены записи
+raise info 'Не найдено';ENDIF;END;$$ language plpgsql;DROP FUNCTION owners_by_func(n_numbuilding numeric, regiment numeric);
