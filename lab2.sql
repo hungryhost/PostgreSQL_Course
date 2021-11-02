4d7da10c5d2b8ee5eb256f3921c3b634e7d14eec
hungryhost <hungryhost@icloud.com>
2021-11-02 06:34:32 +0300

    codestyle fix

--- lab2.sql
+++ lab2.sql
@@ -1,18 +1,18 @@
 /* task 1 */
-CREATE or replace VIEW children_owners(ФИО, Улица, Дом, Квартира, Лет, id)
-AS SELECT c.fio, e.street, e.numbuilding, o.flat, (extract(years from age(current_date, c.born))), o.id
+CREATE OR replace VIEW children_owners(ФИО, Улица, Дом, Квартира, Лет, id)
+AS SELECT c.fio, e.street, e.numbuilding, o.flat, (extract(years FROM age(current_date, c.born))), o.id
 FROM adress e, owners c, ownership o
-WHERE e.idbuiding = o.idbuilding and o.owner = c.id and extract( years from age(current_date, c.born))< 18;
+WHERE e.idbuiding = o.idbuilding AND o.owner = c.id AND extract(years FROM age(current_date, c.born))< 18;
 
 /* task 2*/
-create view owners_by_building(Номер_здания, Улица, Дом, Количество_Собственников)
-as select o.idbuilding, a.street, a.numbuilding, count(o.idbuilding)
-from adress a, ownership o
-where a.idbuiding = o.idbuilding and o.endown is null
-group by o.idbuilding, a.street, a.numbuilding;
+CREATE view owners_by_building(Номер_здания, Улица, Дом, Количество_Собственников)
+AS SELECT o.idbuilding, a.street, a.numbuilding, COUNT(o.idbuilding)
+FROM adress a, ownership o
+WHERE a.idbuiding = o.idbuilding AND o.endown IS NULL
+GROUP BY o.idbuilding, a.street, a.numbuilding;
 
 /* task 3 */
-create view current_owners(Номер_Здания, Квартира, Количество_Собственников)
-as select idbuilding, flat, count(flat) filter ( where endown is null and startown is not null )
-from ownership
-group by idbuilding, flat;
+CREATE view current_owners(Номер_Здания, Квартира, Количество_Собственников)
+AS SELECT idbuilding, flat, COUNT(flat) filter (WHERE endown IS NULL AND startown IS NOT NULL )
+FROM ownership
+GROUP BY idbuilding, flat;
