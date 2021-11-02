974ea1e5644d1ee4ca5bb1d39c8ae41b35b03388
hungryhost <hungryhost@icloud.com>
2021-11-02 06:37:12 +0300

    codestyle format

--- TablesForAirportDB.sql
+++ TablesForAirportDB.sql
@@ -16,81 +16,149 @@
 /*
  * Таблица "Страны"
  */
-create table countries(c_name varchar(50) primary key);
+CREATE TABLE countries
+  (
+     c_name VARCHAR(50) PRIMARY KEY
+  );
 
 /*
  * Таблица "Города"
  */
-create table cities(g_city varchar(50), g_country varchar(50), primary key(g_city, g_country),
-foreign key(g_country) references countries);
+CREATE TABLE cities
+  (
+     g_city    VARCHAR(50),
+     g_country VARCHAR(50),
+     PRIMARY KEY(g_city, g_country),
+     FOREIGN KEY(g_country) REFERENCES countries
+  );
 
 /*
  * Таблица "Аэропорты"
  */
-create table airports(ap_code char(3) primary key, ap_name varchar(50) not null,
- ap_city varchar(50) not null, ap_country varchar(50) not null, foreign key(ap_city, ap_country)
-     references cities(g_city, g_country));
+CREATE TABLE airports
+  (
+     ap_code    CHAR(3) PRIMARY KEY,
+     ap_name    VARCHAR(50) NOT NULL,
+     ap_city    VARCHAR(50) NOT NULL,
+     ap_country VARCHAR(50) NOT NULL,
+     FOREIGN KEY(ap_city, ap_country) REFERENCES cities(g_city, g_country)
+  );
 
 /*
  * Таблица "Департаменты"
 */
- create table departments(d_id numeric(4) primary key, d_name varchar(100) not null, d_type varchar(10)
-     check (d_type in ('внутренний', 'внешний')), d_income numeric(9,2) not null);
+CREATE TABLE departments
+  (
+     d_id     NUMERIC(4) PRIMARY KEY,
+     d_name   VARCHAR(100) NOT NULL,
+     d_type   VARCHAR(10) CHECK (d_type IN ('внутренний',
+     'внешний')),
+     d_income NUMERIC(9, 2) NOT NULL
+  );
 
 /*
  * Таблица "Авиакомпании"
  */
-create table airlines(al_id varchar(20) primary key, al_name varchar(50) not null unique);
+CREATE TABLE airlines
+  (
+     al_id   VARCHAR(20) PRIMARY KEY,
+     al_name VARCHAR(50) NOT NULL UNIQUE
+  );
 
 /*
  * Таблица "Должность-оклад"
  */
-create table postSalary(ps_post varchar(50) primary key, ps_salary numeric(8,2) not null check (ps_salary >= 15000));
+CREATE TABLE postsalary
+  (
+     ps_post   VARCHAR(50) PRIMARY KEY,
+     ps_salary NUMERIC(8, 2) NOT NULL CHECK (ps_salary >= 15000)
+  );
 
 /*
  * Таблица "Модели"
  */
-create table models(m_name varchar(50) primary key);
+CREATE TABLE models
+  (
+     m_name VARCHAR(50) PRIMARY KEY
+  );
 
 /*
  * Табилца "Типы рейсов"
  */
-create table flightTypes(ft_name varchar(30) primary key);
+CREATE TABLE flighttypes
+  (
+     ft_name VARCHAR(30) PRIMARY KEY
+  );
 
 /*
  * Таблица "Самолеты"
  */
-create table airplanes(s_av varchar(20), s_num varchar(20),
- s_model varchar(50) references models(m_name), s_date date not null, s_lastcheck date not null,
- primary key(s_av, s_num), foreign key(s_av) references airlines(al_id));
+CREATE TABLE airplanes
+  (
+     s_av        VARCHAR(20),
+     s_num       VARCHAR(20),
+     s_model     VARCHAR(50) REFERENCES models(m_name),
+     s_date      DATE NOT NULL,
+     s_lastcheck DATE NOT NULL,
+     PRIMARY KEY(s_av, s_num),
+     FOREIGN KEY(s_av) REFERENCES airlines(al_id)
+  );
 
 /*
  * Таблица "Сотрудники
  */
- create table employees(e_dep numeric(4) references departments(d_id), e_id numeric(8) primary key, e_fam varchar(20) not null,
- e_name varchar(20) not null, e_patr varchar(20) not null, e_gender char(1) check (e_gender in ('м', 'ж')),
-  e_dob date not null, e_pass char(10) not null unique, e_given char(12) not null,
- e_date date not null, e_inn char(12) not null unique, e_post varchar(50) references postSalary(ps_post));
+CREATE TABLE employees
+  (
+     e_dep    NUMERIC(4) REFERENCES departments(d_id),
+     e_id     NUMERIC(8) PRIMARY KEY,
+     e_fam    VARCHAR(20) NOT NULL,
+     e_name   VARCHAR(20) NOT NULL,
+     e_patr   VARCHAR(20) NOT NULL,
+     e_gender CHAR(1) CHECK (e_gender IN ('м', 'ж')),
+     e_dob    DATE NOT NULL,
+     e_pass   CHAR(10) NOT NULL UNIQUE,
+     e_given  CHAR(12) NOT NULL,
+     e_date   DATE NOT NULL,
+     e_inn    CHAR(12) NOT NULL UNIQUE,
+     e_post   VARCHAR(50) REFERENCES postsalary(ps_post)
+  );
 
 /*
  * Таблица "Телефоны сотрудников"
  */
- create table phones(t_emp numeric(8) references employees(e_id), t_num varchar(20) primary key);
+CREATE TABLE phones
+  (
+     t_emp NUMERIC(8) REFERENCES employees(e_id),
+     t_num VARCHAR(20) PRIMARY KEY
+  );
 
 /*
  * Таблица "Рейсы"
  */
-create table flights(f_id INTEGER primary key, f_numb numeric(10) not null, f_ser varchar(20) not null,
-f_plane varchar(20) not null, f_airport char(3) not null, f_start timestamp not null, f_end timestamp not null,
-f_type varchar(30) not null, f_incoming boolean not null,
-foreign key(f_ser, f_plane) references airplanes(s_num, s_av),
-foreign key (f_type) references flightTypes(ft_name),
-foreign key (f_airport) references airports(ap_code));
- /*
-  * Таблица "Обслуживание"
-  */
+CREATE TABLE flights
+  (
+     f_id       INTEGER PRIMARY KEY,
+     f_numb     NUMERIC(10) NOT NULL,
+     f_ser      VARCHAR(20) NOT NULL,
+     f_plane    VARCHAR(20) NOT NULL,
+     f_airport  CHAR(3) NOT NULL,
+     f_start    TIMESTAMP NOT NULL,
+     f_end      TIMESTAMP NOT NULL,
+     f_type     VARCHAR(30) NOT NULL,
+     f_incoming BOOLEAN NOT NULL,
+     FOREIGN KEY(f_ser, f_plane) REFERENCES airplanes(s_num, s_av),
+     FOREIGN KEY (f_type) REFERENCES flighttypes(ft_name),
+     FOREIGN KEY (f_airport) REFERENCES airports(ap_code)
+  );
 
-create table service(serv_emp numeric(8), serv_flight
-    INTEGER, PRIMARY KEY(serv_emp, serv_flight),
-    foreign key(serv_emp) references employees(e_id),
-    foreign key (serv_flight) references flights(f_id));
+/*
+ * Таблица "Обслуживание"
+ */
+CREATE TABLE service
+  (
+     serv_emp    NUMERIC(8),
+     serv_flight INTEGER,
+     PRIMARY KEY(serv_emp, serv_flight),
+     FOREIGN KEY(serv_emp) REFERENCES employees(e_id),
+     FOREIGN KEY (serv_flight) REFERENCES flights(f_id)
+  ); 
