bdd5103e5571ed51871261dbeb111e75fafdb3ce
hungryhost <hungryhost@icloud.com>
2021-11-02 06:37:43 +0300

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,30 +1,51 @@
 --- Лабораторная работа 5 ---
 --- Задание 1 (проекция) ---
-SELECT DISTINCT type, ser
-from docs;
+SELECT DISTINCT type,
+                ser
+FROM   docs;
+
 --- Задание 2 (селекция) ---
 SELECT *
-from docs
-WHERE date_and_provider like '%Москвы%';
+FROM   docs
+WHERE  date_and_provider LIKE '%Москвы%';
+
 --- Задание 3 (декартово произведение) ---
-SELECT * from owners, docs;
+SELECT *
+FROM   owners,
+       docs;
+
 --- Задание 4 (объединение) ---
-SELECT * from docs
-UNION SELECT * from old_docs
-order by owner;
+SELECT *
+FROM   docs
+UNION
+SELECT *
+FROM   old_docs
+ORDER  BY owner;
+
 --- Для задания 5 выведем старые документы людей, у которых не зарегестрированы новые ---
 --- Задание 5 (разность с помощью NOT IN) ---
-SELECT owner, type, ser, date_and_provider
-FROM old_docs as oldDocs
-WHERE owner NOT in (select docs.owner
-    from docs where oldDocs.owner = docs.owner);
+SELECT owner,
+       type,
+       ser,
+       date_and_provider
+FROM   old_docs AS oldDocs
+WHERE  owner NOT IN (SELECT docs.owner
+                     FROM   docs
+                     WHERE  oldDocs.owner = docs.owner);
+
 --- Задание 6 (пересечение) (старые документы тех, у кого есть старые и новые документы) ---
-SELECT owner, type, ser, date_and_provider
-FROM old_docs as o_Docs
-WHERE owner IN (select docs.owner
-    from docs where docs.owner = o_Docs.owner);
+SELECT owner,
+       type,
+       ser,
+       date_and_provider
+FROM   old_docs AS o_Docs
+WHERE  owner IN (SELECT docs.owner
+                 FROM   docs
+                 WHERE  docs.owner = o_Docs.owner);
+
 --- Задание 7 (соединение) ---
 SELECT *
-from docs, owners o
-where docs.owner = o.id
-order by sex;
+FROM   docs,
+       owners o
+WHERE  docs.owner = o.id
+ORDER  BY sex; 
